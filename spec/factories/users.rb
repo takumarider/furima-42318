fb070dfa0bcdcf9ad64fcf4791117b2676ce7270
自分の姓名（ローマ字） <takuma.ride@gmail.com>
2025-05-23 22:33:34 +0900

    factorybotの値追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              { 'a1' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    last_name             { '山田' }
+    first_name            { '太郎' }
+    last_name_kana        { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
+    birth_date            { '2000-01-01' }
   end
 end
