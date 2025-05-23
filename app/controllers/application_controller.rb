328c6ce488a657fcd0d2a8fed52049a37a634683
自分の姓名（ローマ字） <takuma.ride@gmail.com>
2025-05-23 20:46:14 +0900

    新規登録、ログイン、ログアウト実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :basic_auth_required?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -15,5 +16,13 @@ class ApplicationController < ActionController::Base
     result = Rails.env.development? || Rails.env.production?
     Rails.logger.info "basic_auth_required? => #{result}, Rails.env: #{Rails.env}"
     result
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date
+    ])
   end
 end
