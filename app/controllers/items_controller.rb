a3b990b25d91c5fe78f28cdb56753f6b14229e71
自分の姓名（ローマ字） <takuma.ride@gmail.com>
2025-05-30 16:21:32 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -20,6 +20,24 @@ class ItemsController < ApplicationController
       redirect_to root_path, notice: '商品を出品しました'
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    return if current_user == @item.user
+
+    redirect_to root_path, alert: '他のユーザーの商品は編集できません'
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.user != current_user
+      redirect_to root_path, alert: '他のユーザーの商品は編集できません'
+    elsif @item.update(item_params)
+      redirect_to item_path(@item), notice: '商品情報を更新しました'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
