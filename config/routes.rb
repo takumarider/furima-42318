eead23322ceb12a08ac8017b197255dbca23530b
自分の姓名（ローマ字） <takuma.ride@gmail.com>
2025-05-30 17:50:35 +0900

    商品削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "items#index"
-  resources :items, only: [:index, :new, :create , :show, :edit, :update,]
-  # resources :users, only: [:show] ← 必要ならendを付けて復活させてください
+  resources :items, only: [:index, :new, :create , :show, :edit, :update, :destroy]
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
-  end
+end
