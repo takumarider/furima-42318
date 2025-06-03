dea0ab6fff262d7f51e98a77eee9eecbad8837b9
自分の姓名（ローマ字） <takuma.ride@gmail.com>
2025-06-03 18:56:10 +0900

    Update 20250524125410_create_items.rb

--- db/migrate/20250524125410_create_items.rb
+++ db/migrate/20250524125410_create_items.rb
@@ -8,7 +8,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer    :status_id,         null: false
       t.integer    :shipping_fee_id,   null: false
       t.integer    :prefecture_id,     null: false
-      t.integer    :shipping_day_id,   null: false
+      t.integer    :shipping_days_id,   null: false
       t.references :user,              null: false, foreign_key: true
 
       t.timestamps
